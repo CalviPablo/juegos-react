3add0865bea0d4ec835bef0cad72bc7224599c17
Pablo <96096465+CalviPablo@users.noreply.github.com>
2022-12-04 13:25:46 -0300

    Update Productos.js

--- src/components/productos/Productos.js
+++ src/components/productos/Productos.js
@@ -4,37 +4,59 @@ import { Link } from "react-router-dom";
 
 export default function Productos() {
   const [products, setProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
     axios.get("http://127.0.0.1:8000/api/products/").then((res) => {
       setProducts(res.data);
-      console.log(res.data);
+      setAllProducts(res.data);
+      // console.log(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/category/").then((res) => {
+      setFilters(res.data);
+      // console.log(res.data);
     });
   }, []);
 
   const handleChange = (e) => {
     let selectedValue = e.target.value;
-    let filteredArray = products.filter((item) => item.type === selectedValue);
-    setProducts(filteredArray);
+    console.log(products);
+    let filteredArray = products.filter(
+      (item) => item.category_id.toString() === selectedValue
+    );
+    console.log(filteredArray);
+    if (selectedValue === "all") {
+      setAllProducts(products);
+    } else {
+      setAllProducts(filteredArray);
+    }
   };
   return (
     <>
       <div className="container">
         <div className="section">
-          <h1 className="mt-md-3 mt-3 text-title">Productos : </h1>
+          <h1 className="mt-md-3 mt-3 text-title">Productos: </h1>
           <hr className="divider" />
           <div className="izquierda">
             <h2 className="mt-md-3 mt-3">Categorias</h2>
             <select onChange={handleChange}>
-              <option value="">All</option>
-              <option value="product">Clothing</option>
-              <option value="electronics">Electronics</option>
-              <option value="furniture">Furniture</option>
+              <option value="all">All</option>
+              {filters.map((filter) => {
+                return (
+                  <option key={filter.id} value={filter.id}>
+                    {filter.name}
+                  </option>
+                );
+              })}
             </select>
             <br />
           </div>
           <div className="row mt-4">
-            {products.map((product) => {
+            {allProducts.map((product) => {
               return (
                 <div className="col-lg-4 col-md-6 col-12 mb-3" key={product.id}>
                   <div className="card">
